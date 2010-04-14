da43cc7175f9b28d43065e299011cf163598a3da
Thomas Markus <github@thomasmarkus.nl>
2010-04-14 22:22:34 +0200

    Extraheer aan wie een kamervraag is gesteld (nogal hackige oplossing, maar werkt best aardig)

--- src/polii/aggregators/bekendmakingen/Bekendmakingen.java
+++ src/polii/aggregators/bekendmakingen/Bekendmakingen.java
@@ -2,10 +2,16 @@ package polii.aggregators.bekendmakingen;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream.GetField;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.AbstractCollection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -23,6 +29,7 @@ import org.joda.time.format.DateTimeFormatter;
 import org.openrdf.model.Resource;
 import org.openrdf.model.URI;
 import org.openrdf.model.ValueFactory;
+import org.openrdf.model.impl.URIImpl;
 import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
@@ -46,6 +53,8 @@ public class Bekendmakingen {
 
 	public static String base = "https://zoek.officielebekendmakingen.nl/";
 	private static String propertyBase = "http://politiek.thomasmarkus.nl/ontology/properties/";
+	private static String entityBase = "http://politiek.thomasmarkus.nl/ontology/entities/";
+	
 	private Repository myRepository;
 	private ValueFactory vf;
 	private static final Resource[] context = new Resource[1];
@@ -154,7 +163,7 @@ public class Bekendmakingen {
 			}
 		
 			//stop requesting pages if there are no more useable hyperlinks on it
-			if (nodes.getLength() == 0) {
+			if (nodes.getLength() == 0){ // || page_nr == 1) { //FIXME: restricted crawling so far
 				System.out.println("No more useable hyperlinks found! Stopping...");
 				getNextPage = false;
 			}
@@ -203,7 +212,26 @@ public class Bekendmakingen {
 			String subject = node.getFirstChild().getNodeValue();
 			con.add(ikregeer_uri, vf.createURI(DC_11.description.toString()), vf.createLiteral(subject), context);
 		}
-
+		
+		
+		//extraheer de tekstvalues aan wie de vraag is gesteld
+		xpath = new DOMXPath("//kamervraagomschrijving");
+		Node textNode = (Node) xpath.selectSingleNode(doc);
+		
+		String text = "";
+		for(int i=0; i < textNode.getChildNodes().getLength(); i++)
+		{
+			text += textNode.getChildNodes().item(i).getNodeValue();
+		}
+		
+		//extraheer aan wie de vraag is gesteld (specifiek voor kamervragen)
+		Set<URI> questionDirectionAt = extractMinisterStaatssecretarissenFromString(text);
+		
+		for(URI bewindspersoon : questionDirectionAt)
+		{
+			con.add(ikregeer_uri, vf.createURI(propertyBase+"questionDirectedAt"), bewindspersoon, context);
+		}
+		
 		xpath = new DOMXPath("//tekstregel[@inhoud='kameraanduiding']");
 		Node kamer = (Node) xpath.selectSingleNode(doc);
 		String kameraanduiding = kamer.getFirstChild().getNodeValue();
@@ -226,4 +254,139 @@ public class Bekendmakingen {
 		
 		System.out.println("------------------------------------");
 	}
+
+	private Set<URI> extractMinisterStaatssecretarissenFromString(String text) {
+
+		/**
+		 * Alleen ministers&Staatssecretarissen: http://www.statengeneraaldigitaal.nl/glossary.html#K
+		 * 
+		 * vb:
+		 * Vragen van het lid (SP) aan de minister van Buitenlandse Zaken over (ingezonden ).
+		 * Vragen van de leden , (beiden PvdA) en (GroenLinks) aan de ministers van Justitie en van Defensie over (ingezonden ).
+		 * Vragen van het lid (CDA) aan de minister van Economische Zaken over (ingezonden ).
+		 * Vragen van het lid (D66) aan de minister van Volksgezondheid, Welzijn en Sport over (ingezonden ).
+		 * Vragen van het lid (SP) aan de minister van Volksgezondheid, Welzijn en Sport over (ingezonden ).
+		 * Vragen van de leden en (beiden PvdA) aan de minister van Volksgezondheid, Welzijn en Sport over (ingezonden ).
+		 * Vragen van de leden , en (allen VVD) aan de ministers van Verkeer en Waterstaat, van Financi‘n en van Volkshuisvesting, Ruimtelijke Ordening en Milieubeheer over (ingezonden ).
+		 * Vragen van de leden en (beiden CDA) aan de ministers van Volkshuisvesting, Ruimtelijke Ordening en Milieubeheer en van Financi‘n over (ingezonden ).
+		 * Vragen van het lid (SP) aan de minister van Verkeer en Waterstaat over (ingezonden ).
+		 * Vragen van het lid (SP) aan de ministers van Verkeer en Waterstaat en van Buitenlandse Zaken over (ingezonden ).
+		 */
+		
+		//algorithm: split op bekende ministeries
+		//altijd links daarvan staat minister/staatssecretaris (inclusive OF) aanduiding	
+		
+		Set<String> ministeries = getMinisteries();
+		Set<String> functies = getBewindsliedenSoorten();
+		//create regular expression of ministeries
+		
+		Pattern pattern = Pattern.compile(join((AbstractCollection<String>) ministeries,"|")+"|"+join((AbstractCollection<String>) functies,"|"),Pattern.CASE_INSENSITIVE );
+		
+		//get the largest matches for this large regular expression of all ministeries
+		Matcher myMatcher = pattern.matcher(text);
+		LinkedList<String> matches = new LinkedList<String>(); 
+		Set<URI> uris = new HashSet<URI>();
+		while (myMatcher.find()) {
+			
+			String extract = text.substring(myMatcher.start(), myMatcher.end());
+			matches.add(extract);
+			
+			if (ministeries.contains(extract)) //naam van ministerie gevonden
+			{
+				//zoek naar dichtsbijzijnde match op minister/staatssecretaris
+				Set<String> localFunctions = new HashSet<String>();
+				int start = -1;
+				for(int i = matches.size()-1; i >= 0 && start == -1; i--)
+				{
+					System.out.println(matches.get(i));
+					
+					if (functies.contains(matches.get(i))) {
+						start = i;
+						localFunctions.add(matches.get(start));
+
+						//kijk verder naar links voor andere functies (bijv minister&staatssecretaris)
+						if (start > 0 && functies.contains(matches.get(start-1))) localFunctions.add(matches.get(start-1));
+					}
+				}
+			
+				for(String f : localFunctions)
+				{
+					uris.add(createPoliticalPostURI(f, extract));
+				}
+			}
+		}
+			
+		return uris;
+	}
+
+	private static URI createPoliticalPostURI(String functie, String ministerie)
+	{
+		// meervoud -> enkelvoud
+		if (functie.equals("Ministers")) functie = "Minister";
+		if (functie.equals("Staatssecretarissen")) functie = "Staatssecretaris";
+		
+		URI post = new URIImpl(entityBase + functie + ministerie.replace(" ", "_"));
+		return post;
+	}
+
+	public static String join(AbstractCollection<String> s, String delimiter) {
+	    if (s.isEmpty()) return "";
+	    Iterator<String> iter = s.iterator();
+	    StringBuffer buffer = new StringBuffer(iter.next());
+	    while (iter.hasNext()) buffer.append(delimiter).append(iter.next());
+	    return buffer.toString();
+	}
+
+	/**
+	 * Get a list of ministeries
+	 * @return
+	 */
+	
+	//FIXME: extract this list from the semantic repository or something instead of hardcoding it!
+	private Set<String> getMinisteries()
+	{
+		Set<String> ministeries = new HashSet<String>();
+		
+		ministeries.add("Justitie");
+		ministeries.add("Buitenlandse Zaken");
+		ministeries.add("Economische Zaken");
+		ministeries.add("Volksgezondheid, Welzijn en Sport");
+		ministeries.add("Verkeer en Waterstaat");
+		ministeries.add("Volkshuisvesting, Ruimtelijke Ordening en Milieubeheer");
+		ministeries.add("Financi‘n");
+		ministeries.add("Defensie");
+		ministeries.add("Algemene Zaken");
+		ministeries.add("Binnenlandse Zaken en Koninkrijksrelaties");
+		ministeries.add("Onderwijs, Cultuur en Wetenschap");
+		ministeries.add("Landbouw, Natuur en Voedselkwaliteit");
+		ministeries.add("Sociale Zaken en Werkgelegenheid");
+		ministeries.add("Volksgezondheid, Welzijn en Sport");
+	
+		return ministeries;
+	}
+	
+	/**
+	 * Vraag alle typen bewindspersonen op
+	 * @return
+	 */
+	
+	private Set<String> getBewindsliedenSoorten()
+	{
+		Set<String> functie = new HashSet<String>();
+		
+		functie.add("Minister");
+		functie.add("Ministers");
+		functie.add("Staatssecretaris");
+		functie.add("Staatssecretarissen");
+		
+		Set<String> functiesLowercase = new HashSet<String>();
+		for(String f : functie)
+		{
+			functiesLowercase.add(f.toLowerCase());
+		}
+		functie.addAll(functiesLowercase);
+		
+		return functie;
+	}
+	
 }
